6f5618e5f916c15841793ecdddbc71de0f708774
Petter2000 <petter@LAPTOP-Q8NLRG1H.localdomain>
2022-09-05 09:50:29 +0200

    Fixed User views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/profile'
+  devise_for :users, controllers: {
+    sessions: 'user/sessions',
+    registrations: 'user/registrations'
+  }
+
+  get '/u/:id', to: 'users#profile', as: 'user'
+
   resources :posts
-  root 'pages#home'
+
   #get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+  root 'pages#home'
   #get 'posts', to: 'pages/post'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+User.create(email: "admin@blog.com", password: "password", password_confirmation: "password")
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body#{x} Words go here")
-end    
+    Post.create(title: "Title #{x}", body: "Body#{x} Words go here", user_id: User.first.id)
+
+end
